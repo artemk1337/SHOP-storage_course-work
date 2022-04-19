e37b9886ebe9c8ac2af5c596d13a9f69cac638c1
Ristle <ristleell@gmail.com>
2022-04-19 17:16:42 +0300

    update test

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,18 @@
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QPixmap
-import sys
+#!/usr/bin/env python3
+# coding --utf-8--
 
+import sys
+from store import settings
+from PyQt5.QtWidgets import QApplication
 from store.main_window.main_window import MainWindow
 from store.widgets.main_table_widget import WinTableData
 
-
 if __name__ == "__main__":
+    settings.InitSettings.update_ui()
     APP = QApplication(sys.argv)
     WIN = MainWindow()
     WIN.show()
 
     TA = WinTableData(WIN)
-
-    pixmap = QPixmap("../banan.jpeg")
-    WIN.preview_img.setPixmap(pixmap)
 
     sys.exit(APP.exec())
